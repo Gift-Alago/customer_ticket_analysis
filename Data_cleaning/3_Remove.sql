f5b5dc1dc3d009c7c460a25e8b2728f9764ce8fa
Gift-Alago <giftalago18@gmail.com>
2025-04-13 18:44:10 +0100

    Update 3_Remove.sql

--- Data_cleaning/3_Remove.sql
+++ Data_cleaning/3_Remove.sql
@@ -1,5 +1,12 @@
 --Remove unnecesaary columns not needed for analysis
 
+columns removed;
+-customer_name
+-ticket_description
+-resolution
+-customer_satisfication_rating
+  
+
 ALTER TABLE customer_support_3
 DROP 
   column customer_name,
